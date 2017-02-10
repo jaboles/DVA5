1a4398b4fb6a342a17a34cdb473ddc7906646c9e
Jonathan Boles <jaboles@fastmail.fm>
2017-02-10 12:07:00 +1100

    5.3.5

--- jb/dvacommon/DVA.java
+++ jb/dvacommon/DVA.java
@@ -48,8 +48,8 @@ public class DVA {
     ArrayList<URL> verifiedUrlList;
     final static Logger logger = LoggerFactory.getLogger(DVA.class);
 
-    public static final String VersionString = "5.3.4";
-    public static final String CopyrightMessage = "Copyright © Jonathan Boles 1999-2016";
+    public static final String VersionString = "5.3.5";
+    public static final String CopyrightMessage = "Copyright © Jonathan Boles 1999-2017";
 
     // Keep track of the applications own jars so that they don't get treated as sound libraries.
     public static final String[] OWN_JARS_Array = new String[] {
