8c3317cb1815aefd64b4e8ed63e8de70982f20ec
Jonathan Boles <jaboles@fastmail.fm>
2022-02-21 00:01:10 -0800

    Update notarize script to use password from ADO

--- notarize.sh
+++ notarize.sh
@@ -8,13 +8,21 @@ if [[ $1 =~ "app" ]]; then
 else
 	notarizebinary=$1
 fi
+
+password=''
+if [[ "$SYSTEM_COLLECTIONURI" == "https://jonathanboles.visualstudio.com/" ]]; then
+  password="@env:DVANotarizationPassword"
+else
+  password="@keychain:DVAnotarization"
+fi
+
 echo Sending $notarizebinary to Apple
-output=`xcrun altool --notarize-app -f $notarizebinary --primary-bundle-id jb.dva -t osx -u jonno@fastmail.fm -p @keychain:DVAnotarization 2>&1`
+output=`xcrun altool --notarize-app -f $notarizebinary --primary-bundle-id jb.dva -t osx -u jonno@fastmail.fm -p $password 2>&1`
 echo output is ${output}
 uuid=`echo ${output} | tail -1 | rev | cut -d' ' -f1 | rev`
 echo UUID is ${uuid}
 while true; do
-	output=`xcrun altool --notarization-info $uuid -u jonno@fastmail.fm -p @keychain:DVAnotarization 2>&1`
+	output=`xcrun altool --notarization-info $uuid -u jonno@fastmail.fm -p $password 2>&1`
 	echo $output
 	if [[ $output =~ "Status: success" ]]; then
 		break
